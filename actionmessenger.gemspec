1c8c526fae759878f72fef3091f598648149ccda
Tim Walsh <tim@mims.ms>
2019-06-22 02:51:20 -0700

    Adds Twilio and Required ENV Variables

--- actionmessenger.gemspec
+++ actionmessenger.gemspec
@@ -38,6 +38,8 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ['lib']
 
+  spec.add_dependency 'twilio-ruby', '~> 5.22.0'
+
   spec.add_development_dependency 'bundler', '~> 2.0'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
--- lib/action_messenger.rb
+++ lib/action_messenger.rb
@@ -1,5 +1,10 @@
+require 'twilio-ruby'
 require 'action_messenger/version'
 
+# ActionMessenger allows you to send SMS (text-messages) from your application
+# using a basic "messenger" model and associated .text.erb view files.
+#
+# It is heavily modeled after ActionMailer and attempts to provide a reliable
+# and extensible core interface for managing text-message notifications.
 module ActionMessenger
-  class Error < StandardError; end
 end
