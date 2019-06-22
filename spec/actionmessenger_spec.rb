7983b9a6e0a91fa8c750d2dfb9c39de7ac686095
Tim Walsh <tim@mims.ms>
2019-06-22 01:00:19 -0700

    Commits Gemfile.lock and Removes Impossible Spec

--- spec/actionmessenger_spec.rb
+++ spec/actionmessenger_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Actionmessenger do
   it "has a version number" do
     expect(Actionmessenger::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
